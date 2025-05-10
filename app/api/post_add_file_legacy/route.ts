c66f7cd9a7f4e8eda5c95df4493cea4c6f1c4742
Frederic Kuhwald <frederic@kuhwald-holding.de>
2025-05-10 19:36:09 +0200

    modifies post_add_file to take base64 string

--- app/api/post_add_file_legacy/route.ts
+++ app/api/post_add_file_legacy/route.ts
@@ -2,23 +2,32 @@ import {NextResponse} from 'next/server';
 import {create_file} from "@/app/services/supabase/supabase";
 import {classify_file_type} from "@/app/services/alvi_ai/classify_file_type";
 
+
 export async function POST(request: Request): Promise<NextResponse> {
     try {
         console.log(request);
-
         // Get the form data from the request
-        const requestBody: any = await request.json();
-        console.log("requestBody",requestBody);
-        const patientId: string = requestBody.patientId;
-        const file_data: { file_name: string, file_type: string, file_base64: string } = requestBody.file_data;
+        const formData = await request.formData();
+        const file = formData.get('image') as File;
+        const patientId = formData.get('patientId') as string;
 
-        // convert base64 to file buffer
-        const buffer: Buffer = Buffer.from(file_data.file_base64, 'base64');
+        console.log(formData);
 
+        if (!file) {
+            return NextResponse.json(
+                {error: 'No file provided'},
+                {status: 400}
+            );
+        }
+
+
+        // Convert file to base64
+        const bytes: ArrayBuffer = await file.arrayBuffer();
+        const buffer: Buffer<ArrayBuffer> = Buffer.from(bytes);
 
         // Define filename: Current timestamp (seconds) + file name
         const timestamp: number = Math.floor(Date.now() / 1000);
-        const fileName: string = `${timestamp}_${file_data.file_name}`;
+        const fileName: string = `${timestamp}_${file.name}`;
 
         // Upload to Blob storage + create a new entry in documents table
         const file_response: any = await create_file(fileName, buffer, patientId);
@@ -29,7 +38,9 @@ export async function POST(request: Request): Promise<NextResponse> {
         // For now, we'll just return a success response
         return NextResponse.json({
             success: true,
-            file_data: {file_name: file_data.file_name, file_type: file_data.file_type, file_base64: file_data.file_base64.substring(0, 50) + "..."},
+            fileName: file.name,
+            fileType: file.type,
+            fileSize: file.size,
             file_response: file_response,
             file_classification: file_classification,
             message: 'File received'
